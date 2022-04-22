338977953842676e5bcf769776c315ae34fef976
wkgcass <wkgcass@hotmail.com>
2022-04-22 17:42:47 +0800

    dev: 1.1.0-dev

--- api/src/main/java/io/vproxy/pojoagent/api/Version.java
+++ api/src/main/java/io/vproxy/pojoagent/api/Version.java
@@ -1,8 +1,8 @@
 package io.vproxy.pojoagent.api;
 
 public class Version {
-    public static final String VERSION = "1.0.0"; // _THE_VERSION_
-    public static final long VERSION_INT = 1_000_000L;
+    public static final String VERSION = "1.1.0-dev"; // _THE_VERSION_
+    public static final long VERSION_INT = -1_100_000L;
 
     private Version() {
     }
