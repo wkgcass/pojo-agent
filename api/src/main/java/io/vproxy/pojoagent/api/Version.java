992d64f589bea32ca0af3c7087a53bfcd17b78fe
wkgcass <wkgcass@hotmail.com>
2022-04-22 20:17:56 +0800

    fix VERSION_INT value, and add stringify helper

--- api/src/main/java/io/vproxy/pojoagent/api/Version.java
+++ api/src/main/java/io/vproxy/pojoagent/api/Version.java
@@ -1,9 +1,25 @@
 package io.vproxy.pojoagent.api;
 
+@SuppressWarnings("UnusedReturnValue")
 public class Version {
     public static final String VERSION = "1.1.0-dev"; // _THE_VERSION_
-    public static final long VERSION_INT = -1_100_000L;
+    public static final long VERSION_INT = -1_001_000L;
 
     private Version() {
+    }
+
+    public static String stringifyVersion(long ver) {
+        boolean isDev = ver < 0;
+        if (isDev) {
+            ver = -ver;
+        }
+        long maj = ver / 1_000_000;
+        long min = (ver % 1_000_000) / 1_000;
+        long patch = ver % 1_000;
+        String str = maj + "." + min + "." + patch;
+        if (isDev) {
+            str += "-dev";
+        }
+        return str;
     }
 }
