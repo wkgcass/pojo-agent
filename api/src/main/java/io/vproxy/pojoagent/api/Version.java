7c48a3f17dadebe59d836fed4592da62d7131607
wkgcass <wkgcass@hotmail.com>
2022-04-27 16:42:01 +0800

    prepare for 1.1.0 release

--- api/src/main/java/io/vproxy/pojoagent/api/Version.java
+++ api/src/main/java/io/vproxy/pojoagent/api/Version.java
@@ -2,8 +2,8 @@ package io.vproxy.pojoagent.api;
 
 @SuppressWarnings("UnusedReturnValue")
 public class Version {
-    public static final String VERSION = "1.1.0-dev"; // _THE_VERSION_
-    public static final long VERSION_INT = -1_001_000L;
+    public static final String VERSION = "1.1.0"; // _THE_VERSION_
+    public static final long VERSION_INT = 1_001_000L;
 
     private Version() {
     }
